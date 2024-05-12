409ad4f2335ee5b75f6da38c810373acec65439b
Sayed Shaban | Ejada Systems Ltd. <sshabaan@ejadacairo.local>
2024-05-12 15:32:24 +0300

    some changes

--- Field-Booking-System/src/app/app.routes.ts
+++ Field-Booking-System/src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { ProfileComponent } from './Components/profile/profile.component';
 import { UpdateComponent } from './Components/update/update.component';
 import { FavoritesComponent } from './Components/favorites/favorites.component';
 import { BookingComponent } from './Components/booking/booking.component';
-import { FeildsComponent } from './components/feilds/feilds.component';
-import { FeildDetailsComponent } from './components/feild-details/feild-details.component';
+import { FeildsComponent } from './Components/feilds/feilds.component';
+import { FeildDetailsComponent } from './Components/feild-details/feild-details.component';
 
 export const routes: Routes = [
     {path:"profile",component:ProfileComponent},
