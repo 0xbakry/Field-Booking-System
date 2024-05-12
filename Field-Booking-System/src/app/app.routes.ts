09ea9ea242d1b7568170aa225dbd3e72d63c4e63
Omar ihab | Ejada Systems Ltd. <oihab@ejadacairo.local>
2024-05-12 15:55:17 +0300

    last

--- Field-Booking-System/src/app/app.routes.ts
+++ Field-Booking-System/src/app/app.routes.ts
@@ -5,13 +5,19 @@ import { FavoritesComponent } from './Components/favorites/favorites.component';
 import { BookingComponent } from './Components/booking/booking.component';
 import { FeildsComponent } from './Components/feilds/feilds.component';
 import { FeildDetailsComponent } from './Components/feild-details/feild-details.component';
+import { HomeComponent } from './Components/home/home.component';
+import { FieldtpageComponent } from './Components/fieldtpage/fieldtpage.component';
 
 export const routes: Routes = [
+    {path:"",component:HomeComponent},
+    {path:"home",component:HomeComponent},
+    {path:"fieldtypes",component:FieldtpageComponent},
     {path:"profile",component:ProfileComponent},
     {path:"profile/update-profile", component:UpdateComponent},
     {path:"profile/favorites-fields", component:FavoritesComponent},
     {path:"profile/bookings", component:BookingComponent},
-    {path:"feilds",component:FeildsComponent},
-    {path:"feilds/:id",component:FeildDetailsComponent}
+    {path:"fields",component:FeildsComponent},
+    {path:"fields/:id",component:FeildDetailsComponent}
+
 ];
 
