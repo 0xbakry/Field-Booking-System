5ffe9facfa3f3d644db46353b53aae404d6a611a
Sayed Shaban | Ejada Systems Ltd. <sshabaan@ejadacairo.local>
2024-05-12 18:08:14 +0300

    add filter sort to the fields

--- Field-Booking-System/src/app/Components/feilds/feilds.component.ts
+++ Field-Booking-System/src/app/Components/feilds/feilds.component.ts
@@ -3,6 +3,8 @@ import { FeildsService } from '../../sevices/feilds.service';
 import { FeildComponent } from '../feild/feild.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-feilds',
@@ -10,7 +12,9 @@ import { RouterModule } from '@angular/router';
   imports: [
     FeildComponent,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    CommonModule
   ],
   providers : [
     FeildsService
@@ -22,17 +26,42 @@ import { RouterModule } from '@angular/router';
 export class FeildsComponent implements OnInit {
   
   feilds:any;
+  filteredFields: any;
 
   constructor(private service: FeildsService){}
   
   ngOnInit(): void {
-    this.service.getAllFeilds().subscribe({
-      next:(data)=>{
-        this.feilds = data;
-        console.log(data);
-      },
-      error:(err)=>{console.log("Error")}
-    })
+    this.filteredFields = this.feilds;
+    this.fetchFeilds();
   }
 
+  fetchFeilds() {
+    this.service.getAllFeilds().subscribe({
+      next: (data) => {
+        this.feilds = data;
+        this.filteredFields = [...this.feilds];
+      },
+      error: (err) => { console.log("Error fetching fields"); }
+    });
+  }
+
+  sort(selectedValue: string) {
+    this.applyFilters(selectedValue);
+  }
+  
+  
+  applyFilters(filter: string) {
+    let filtered = [...this.feilds];
+  
+    if (filter === "Low To High") {
+      filtered.sort((a, b) => a.price - b.price);
+    } else if (filter === "High To Low") {
+      filtered.sort((a, b) => b.price - a.price);
+    }
+    else if (filter !== 'reset') {
+      filtered = filtered.filter(field => filter === field.type);
+    }
+  
+    this.filteredFields = filtered;
+  }
 }
