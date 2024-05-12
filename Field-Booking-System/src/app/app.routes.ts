124efca60a434c8ef520b9e24c986aef21230d05
Sayed Shaban | Ejada Systems Ltd. <sshabaan@ejadacairo.local>
2024-05-12 03:09:52 +0300

    Feilds and feild

--- Field-Booking-System/src/app/app.routes.ts
+++ Field-Booking-System/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { ProfileComponent } from './Components/profile/profile.component';
 import { UpdateComponent } from './Components/update/update.component';
 import { FavoritesComponent } from './Components/favorites/favorites.component';
 import { BookingComponent } from './Components/booking/booking.component';
+import { FeildsComponent } from './components/feilds/feilds.component';
+import { FeildDetailsComponent } from './components/feild-details/feild-details.component';
 
 export const routes: Routes = [
     {path:"profile",component:ProfileComponent},
     {path:"profile/update-profile", component:UpdateComponent},
     {path:"profile/favorites-fields", component:FavoritesComponent},
-    {path:"profile/bookings", component:BookingComponent}
+    {path:"profile/bookings", component:BookingComponent},
+    {path:"feilds",component:FeildsComponent},
+    {path:"feilds/:id",component:FeildDetailsComponent}
 ];
+
---
+++ Field-Booking-System/src/app/sevices/feilds.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FeildComponent } from '../components/feild/feild.component';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeildsService {
+
+  constructor(private readonly http:HttpClient) { }
+  private readonly DB_URL = "http://localhost:3000/feilds";
+
+  getAllFeilds(): Observable<FeildComponent[]> {
+    return this.http.get<FeildComponent[]>(this.DB_URL);
+  }
+  getFeildByID(id:number){
+    return this.http.get(this.DB_URL+"/"+id);
+  }
+
+}
