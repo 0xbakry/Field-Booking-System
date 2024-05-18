a18695e3b7a41e47187619f990e21f40b986546a
kambosha <61192867+sayedshaaban4@users.noreply.github.com>
2024-05-18 15:06:32 +0300

    added total price at bookings page

--- Field-Booking-System/src/app/Components/booking/booking.component.ts
+++ Field-Booking-System/src/app/Components/booking/booking.component.ts
@@ -25,6 +25,7 @@ export class BookingComponent implements OnInit {
   user: any;
   userid:any;
   checkUser:any;
+  total=0;
 
   constructor(private serviceF: FeildsService, private serviceU:UsersService) { }
 
@@ -34,19 +35,21 @@ export class BookingComponent implements OnInit {
     this.serviceU.getUser(this.userid).subscribe({
       next: (data1) => {
         this.user = data1;
-        this.bookedFieldsID=this.user.bookings; 
+        this.bookedFieldsID=this.user.bookings;
         this.serviceF.getAllFeilds().subscribe({
           next: (data2) => {
             this.fields = data2;
 
-            // for TOTAL price
-            // this.bookedFields = this.fields.filter((field: any) => {
-              // return this.bookedFieldsID.some((booking: any) => booking.id === field.id);
-          // });
-          
+            for(let i = 0 ; i<this.bookedFieldsID.length ;i++){
+              for(let j=0 ; j<this.fields.length ;j++){
+                  if(this.bookedFieldsID[i].id==this.fields[j].id) {
+                    this.total+=this.fields[j].price;
+                  }
+                }
+              }
         },
           error: (err) => { console.log("Error fetching fields"); }
-        });   
+        });
       },
       error: (err) => { console.log("Error fetching user"); }
     });
@@ -54,7 +57,6 @@ export class BookingComponent implements OnInit {
 
   Pay(id:any , slot:any): void{
     this.serviceU.removeBooking(this.userid,{"id":id,"slot":slot});
-    // this.serviceF.returnFeild(id,slot);
     window.location.reload();
   }
 
